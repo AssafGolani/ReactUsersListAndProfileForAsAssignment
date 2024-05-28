d69825085315ef630edacccbbb9b330eedd694f9
Assaf <1214assaf@gmail.com>
2024-05-28 11:34:26 +0300

    refactor: Update ThreeDViewer component to import useState, useEffect, and useRef from "react"

--- src/components/ThreeDViewer.jsx
+++ src/components/ThreeDViewer.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-case-declarations */
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import * as THREE from "three";
 
 const App = () => {
