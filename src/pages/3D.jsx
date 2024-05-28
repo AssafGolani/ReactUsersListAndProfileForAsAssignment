744b94aa01b90387aa25f5bd1f913675997082f7
Assaf <1214assaf@gmail.com>
2024-05-28 11:28:08 +0300

    feat: Add 3D page with ThreeDViewer component for rendering 3D objects

---
+++ src/pages/3D.jsx
@@ -0,0 +1,10 @@
+import ThreeDViewer from "../components/ThreeDViewer";
+
+export default function ThreeDPage() {
+  return (
+    <>
+      <h2>3D Objects:</h2>
+      <ThreeDViewer />
+    </>
+  );
+}
